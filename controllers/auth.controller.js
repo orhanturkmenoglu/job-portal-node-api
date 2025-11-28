6b8a422871459a51a7b5811d0e229493924e29fb
orhan <orhantrkmn749@gmail.com>
2025-11-29 02:09:04 +0300

    feat: add user registration with validation and detailed logging

---
+++ controllers/auth.controller.js
@@ -0,0 +1,55 @@
+const User = require("../models/user.model");
+
+exports.registerController = async (req, res) => {
+  console.log("📩 Incoming Register Request:");
+  console.log("Body:", req.body);
+  const { firstName, lastName, email, password } = req.body;
+
+  console.log("Request Body : s", req.body);
+
+  if (!firstName || !lastName || !email || !password) {
+    console.warn("⚠ Missing required fields");
+    return res.status(400).json({
+      success: false,
+      message: "All fields (firstName, lastName, email, password) are required",
+    });
+  }
+  try {
+    console.log("🔍 Checking if user already exists with email:", email);
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.warn("⚠ User already exists:", email);
+      return res.status(409).json({
+        success: false,
+        message: "Email is already registered",
+      });
+    }
+    console.log("🆕 Creating new user document...");
+
+    const newUser = new User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    console.log("✅ User created successfully:", newUser._id);
+
+    await newUser.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    console.error("❌ Error in registerController:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const colors = require("colors");
 const connectDB = require("./config/db");
 const testRoutes = require("./routes/test.routes");
+const authRoutes = require ("./routes/auth.routes");
 const morgan = require("morgan");
 require("dotenv").config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/test", testRoutes);
+app.use("/api/v1/auth",authRoutes);
 
 connectDB().then(() => {
   const PORT = process.env.PORT || 5173;
